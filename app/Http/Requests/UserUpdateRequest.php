081c6d2313cf31c0b9c4c94515279f2f628f6c64
otbosarov <abdullohotbosarov720@gmail.com>
2024-12-29 12:08:31 +0500

    User request o'zgartirildi

--- app/Http/Requests/UserUpdateRequest.php
+++ app/Http/Requests/UserUpdateRequest.php
@@ -25,24 +25,31 @@ class UserUpdateRequest extends FormRequest
         return [
             'full_name' => 'required|string|min:1|max:50',
             'password' => 'required|string|min:3',
-            'phone' => 'required|starts_with:+998|size:17|string',
-            'phone' => [Rule::unique('users')->where(function($query){
-                $query->where('phone', $this->phone)->where('id', '!=',$this->id);
-            })]
+            'phone' => [
+            'required',
+            'starts_with:+998',
+            'size:17',
+            'string',
+            Rule::unique('users')->ignore($this->id),
+        ],
+            // 'phone' => 'required|starts_with:+998|size:17|string',
+            // 'phone' => [Rule::unique('users')->where(function($query){
+            //     $query->where('phone', $this->phone)->where('id', '!=',$this->id);
+            // })]
         ];
     }
     public function messages(){
         return [
-            'full_name.required' => 'To\'liq ism kiritish majburiy!',
-            'full_name.min:1' => 'To\'liq ismni minimal qiymati 1 ta belgidan ortiq bo\'lishi kerak ',
-            'full_name.max:60' => 'To\'liq ismni maxsimal qiymati 50 ta belgidan ortiq bo\'lmasligi kerak',
-            'password.required' => 'Parol kiritish majburiy!',
-            'password.min' => 'Parolni minimal qiymati 3 ta belgidan ortiq bolishi kerak',
-            'password.max' => 'Parolni maxsimal qiymati 50 ta belgidan ortiq bolmasligi kerak',
-            'phone.required' => 'Telefon nomer kiriting',
-            'phone.starts_with' => 'Telefon raqamning boshi  +998 boshlanishi kerak ',
-            'phone.size' => 'Telefon nomer kiritish maydoni 17 ta belgidan iborat bolishii kerak ',
-            'phone.unique' => 'Bu raqam allaqachon mavjud'
+           'full_name.required' => 'To\'liq ism kiritish majburiy!',
+        'full_name.min' => 'To\'liq ismning minimal uzunligi 1 ta belgidan ortiq bo\'lishi kerak.',
+        'full_name.max' => 'To\'liq ismning maksimal uzunligi 50 ta belgidan oshmasligi kerak.',
+        'password.required' => 'Parol kiritish majburiy!',
+        'password.min' => 'Parolning minimal uzunligi 3 ta belgidan ortiq bo\'lishi kerak.',
+        'password.max' => 'Parolning maksimal uzunligi 50 ta belgidan oshmasligi kerak.',
+        'phone.required' => 'Telefon raqamini kiritish majburiy!',
+        'phone.starts_with' => 'Telefon raqami +998 bilan boshlanishi kerak.',
+        'phone.size' => 'Telefon raqami uzunligi 17 ta belgidan iborat bo\'lishi kerak.',
+        'phone.unique' => 'Bu raqam allaqachon mavjud.',
         ];
     }
 }
